46738effc1064006ec0a3999e143a9d7081e547f
Tiago Souza <tiago@explorethecuriosity.com>
2020-03-20 09:44:04 -0300

    Remove itens desnecessarios

--- playground/src/index.js
+++ playground/src/index.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import '../../src/DatePicker.css';
+import './fonts/fonts.css';
+import './DatePicker.css';
 import DatePicker from '../../src';
 import * as serviceWorker from './serviceWorker';
 
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import babel from 'rollup-plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import copy from 'rollup-plugin-copy';
 import { terser } from 'rollup-plugin-terser';
 
 const config = {
@@ -18,9 +17,6 @@ const config = {
     }),
     resolve(),
     commonjs(),
-    copy({
-      targets: [{ src: 'src/DatePicker.css', dest: 'lib' }],
-    }),
     terser(),
   ],
 };
