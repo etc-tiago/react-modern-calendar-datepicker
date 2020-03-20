aed165e7901397459524f44419c3ee46700d4635
Tiago Souza <tiago@explorethecuriosity.com>
2020-03-20 09:46:32 -0300

    Change folder to dist

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import { terser } from 'rollup-plugin-terser';
 const config = {
   input: 'src/index.js',
   output: {
-    file: 'lib/index.js',
+    file: 'dist/index.js',
     format: 'cjs',
     exports: 'named',
   },
